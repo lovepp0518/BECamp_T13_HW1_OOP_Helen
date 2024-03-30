3f9db72a3ab63fe6c67d719a0e3113f06889c800
lovepp0518 <lovepp0518@gmail.com>
2024-03-30 14:06:33 +0800

    feat: add defense and adjust attack

--- classes/character.class.php
+++ classes/character.class.php
@@ -16,15 +16,13 @@ class Character
   {
     $criticalHitFactor = rand(1, 100);
     if (($this->luckValue === 1 && $criticalHitFactor <= 10) ||  ($this->luckValue === 2 && $criticalHitFactor <= 20) || ($this->luckValue === 3 && $criticalHitFactor <= 30) || ($this->luckValue === 4 && $criticalHitFactor <= 40) || ($this->luckValue === 5 && $criticalHitFactor <= 50)) {
-      $target->healthPoint -= ($this->physicalAttack) * 2;
+      $target->healthPoint = $target->healthPoint - (($this->physicalAttack * 5) * 2 - $target->physicalDefense);
       echo "\n";
       echo '物理攻擊發動！產生爆擊傷害加倍！' . "\n";
-      echo "{$this->name} 攻擊 {$target->name}. {$target->name} 減少血量 {$this->physicalAttack}*2，{$target->name} 目前血量 {$target->healthPoint}.\n";
     } else {
-      $target->healthPoint -= $this->physicalAttack;
+      $target->healthPoint = $target->healthPoint - (($this->physicalAttack * 5) - $target->physicalDefense);
       echo "\n";
       echo '物理攻擊發動！' . "\n";
-      echo "{$this->name} 攻擊 {$target->name}. {$target->name} 減少血量 {$this->physicalAttack}，{$target->name} 目前血量 {$target->healthPoint}.\n";
     }
     sleep(1);
   }
@@ -33,11 +31,10 @@ class Character
   {
     // 若本身魔力量大於最小魔力消耗值(先固定為一次魔力攻擊消耗2點魔力)
     if ($this->magicValue >= 2) {
-      $target->healthPoint -= $this->magicalAttack;
+      $target->healthPoint = $target->healthPoint - (($this->magicalAttack * 5) - $target->magicalDefense);
       $this->magicValue -= 2;
       echo "\n";
       echo '魔法攻擊發動！' . "\n";
-      echo "{$this->name} 攻擊 {$target->name}. {$target->name} 減少血量 {$this->magicalAttack}，{$target->name} 目前血量 {$target->healthPoint}.\n";
     } else {
       echo '無效攻擊！魔力量不足無法發動魔法攻擊！' . "\n";
     }
