d763ac8ed36bd60237d34603e378b26a224e0740
lovepp0518 <lovepp0518@gmail.com>
2024-04-16 11:08:07 +0800

    test: use pest to test

---
+++ Tests/Unit/generateEnemyNamesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+  use Classes\Enemy;
+
+it('generates 10 unique enemy names', function () {
+// Arrange
+$enemyNames = Enemy::generateEnemyNames();
+
+// Act
+$uniqueEnemyNames = array_unique($enemyNames);
+
+// Assert
+expect(count($uniqueEnemyNames))->toBe(10);
+});
+
---
+++ Tests/Unit/generateEnemyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Classes\Enemy;
+
+it('generates 10 unique enemy names', function () {
+  // Arrange
+  $enemyNames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  $gameLevel = 1;
+
+  // Act
+  $enemy = new Enemy($enemyNames, $gameLevel);
+
+  // Assert
+  expect($enemy->name)->toBe('a');
+});
